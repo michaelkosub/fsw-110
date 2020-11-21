d20c9633e71c961081e6824e86a04c056691c7ab
michaelkosub/fsw-100 <michael.kosub@bryanuniversity.edu>
2020-11-21 15:12:19 -0600

    second commit

--- week2/index1.js
+++ week2/index1.js
@@ -5,8 +5,6 @@ newh1.style.textAlign = "center"
 newh1.style.fontSize = "30px"
 
 
-
-
 var newLi = document.createElement("li")
 newLi.textContent = "Hello how are you doing"
 newLi.style.fontSize = "20px"
@@ -25,8 +23,8 @@ document.body.append(newLi)
 var newp = document.createElement("p")
  newp.textContent = "All of this was created with Javascript"
  document.body.append(newp)
-//  new.style.textAlign = "center"
-//  new.style.fontSize = "30px"
+ newp.style.textAlign = "center"
+ newp.style.fontSize = "30px"
 
  
  
