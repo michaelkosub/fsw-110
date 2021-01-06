ad0fd8a327866442ace231cc85e8d228b9bf50e1
michaelkosub/fsw-100 <michael.kosub@bryanuniversity.edu>
2021-01-06 11:24:40 -0500

    second commit

---
+++ week7/project/index.js
@@ -0,0 +1,36 @@
+var form = document.myform
+console.log(form)
+
+function cap(first){
+    console.log(first)
+    return first[0].toUpperCase()+ first.slice(1)
+    
+}
+
+var button = document.getElementById("submit")
+console.log(button)
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    console.log(form.First.value, form.Second.value)
+    var list = document.getElementById("list")
+    console.log(list)
+    var Li = document.createElement("li")
+    list.append(Li)
+    Li.textContent =cap (form.First.value )+ " " +cap(form.Second.value)
+
+    var del = document.createElement("button")
+    Li.append(del)
+    del.textContent = "Delete"
+   
+    // del.addEventListener("onclick", (e) => {
+    //     e.p
+    // })
+
+    del.addEventListener("submit", (e) => {
+        e.preventDefault()
+        console.log("hello")
+        // console.log(form.First, form.Second)
+
+})
+})
